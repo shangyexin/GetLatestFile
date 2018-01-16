52a583a1bc4a5f46c9c93fbaf0f2edd4100d189d
yasin <yexin.shang@gmail.com>
2018-01-16 16:56:41 +0800

    change the way of instantiating a class

--- HMI.py
+++ HMI.py
@@ -5,15 +5,16 @@
 # @File   : HMI.py
 
 import sys
-from PyQt5.QtWidgets import *
 from mainWindow import *
 
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent=parent)
+        self.setupUi(self)
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    ex = Ui_MainWindow()
-    w = QtWidgets.QMainWindow()
-    ex.setupUi(w)
+    w = MainWindow()
     w.show()
     sys.exit(app.exec_())
